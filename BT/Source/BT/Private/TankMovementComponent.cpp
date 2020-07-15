6b8cce1b5449895322195a4d9922f9d3c3ac1188
mKimpa <mkimpa@gmail.com>
2020-07-15 14:17:31 +0300

    DotProduct - вражеский танк начинает движение

--- BT/Source/BT/Private/TankMovementComponent.cpp
+++ BT/Source/BT/Private/TankMovementComponent.cpp
@@ -29,5 +29,9 @@ void UTankMovementComponent::IntendTurnRight(float Throw)
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
 	auto TankName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s is moving on %s direction"), *TankName, *MoveVelocity.ToString());
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+	IntendMoveForward(ForwardThrow);
+	UE_LOG(LogTemp, Warning, TEXT("%s throw is: %f"), *TankName, ForwardThrow);
 }
